14fddd84c8c92c09e2c70ea8d3656e5368020d65
unknown <lailaibraheem@hotmail.com>
2022-06-13 23:16:42 +0300

    done

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -31,3 +31,40 @@ def test_includes():
     actual = ll.includes(5)
     expected = True
     assert actual == expected
+
+def test_insert_before():
+    ll.insert_before(1,2)
+    actual = ll.toString()
+    expected = [2, 5, 4, 3, 6, 5, 3]
+    assert actual == expected
+
+def test_insert_before_middle():
+    ll.insert_before(4, 7)
+    actual = ll.toString()
+    expected = [2, 5, 4, 7, 3, 6, 5, 3]
+    assert actual == expected
+
+def test_insert_after():
+    ll.insert_after(2,1)
+    actual = ll.toString()
+    expected = [2, 5, 1, 4, 7, 3, 6, 5, 3]
+    assert actual == expected
+
+def test_append_multi_values():
+    ll.append(2)
+    ll.append(3)
+    actual = ll.toString()
+    expected = [2, 5, 1, 4, 7, 3, 6, 5, 3, 2, 3]
+    assert actual == expected
+
+def test_insert_after_middle():
+    ll.insert_after(5,1)
+    actual = ll.toString()
+    expected = [2, 5, 1, 4, 7, 1, 3, 6, 5, 3, 2, 3]
+    assert actual == expected
+
+def test_insert_after_last():
+    ll.insert_after(12,8)
+    actual = ll.toString()
+    expected = [2, 5, 1, 4, 7, 1, 3, 6, 5, 3, 2, 3, 8]
+    assert actual == expected
